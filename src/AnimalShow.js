14593a7745c932d6df831123c51e9372f0b2487f
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-07-05 18:26:35 +0530

    loading and showing svgs

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import bird from './images/bird.svg'
+import cat from './images/cat.svg'
+import cow from './images/cow.svg'
+import dog from './images/dog.svg'
+import gator from './images/gator.svg'
+import heart from './images/heart.svg'
+import horse from './images/horse.svg'
+
+const svgMap ={
+  bird,
+  cat,
+  cow, 
+  dog,
+  gator , 
+  horse,
+}
 
 function AnimalShow({type}) {
   return (
-    <div>{type}</div>
+    <div>
+      <img src = {svgMap[type]} alt = "animal"/>  
+    </div>
   )
 }
 
